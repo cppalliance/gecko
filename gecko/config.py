52019dd8c01e3eaf59bd7a58dc52023fa5a8a0d0
Mohammad Nejati <ashtumashtum@gmail.com>
2023-06-12 10:43:28 +0330

    Add local config (#39)

--- gecko/config.py
+++ gecko/config.py
@@ -15,6 +15,12 @@ config_schema = Schema({
 
 config = {}
 
-with open("./config/config.yaml", "r", encoding='utf-8') as file:
+config_path = "./config/config.local.yaml"
+
+# try to load local config if exists
+if not os.path.exists(config_path):
+    config_path = "./config/config.yaml"
+
+with open(config_path, "r", encoding='utf-8') as file:
     config = yaml.safe_load(file)
     config_schema.validate(config)
