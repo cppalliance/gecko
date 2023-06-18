56b679868051d28b23df2e11d639206491a10b47
Mohammad Nejati <ashtumashtum@gmail.com>
2023-06-18 14:09:26 +0330

    Warp long strings in mobile devices (#67)

--- lab/demo/src/Search.js
+++ lab/demo/src/Search.js
@@ -84,6 +84,7 @@ function CustomHit(hit, url_prefix) {
     <Box
       key={objectID}
       sx={{
+        wordWrap: 'break-word',
         '& mark': {
           color: 'inherit',
           bgcolor: 'inherit',
