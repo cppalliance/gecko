07338887323c003addd488c5755d08b40cda2c1c
Mohammad <ashtumashtum@gmail.com>
2023-10-19 11:37:26 +0000

    Use external search button

--- lab/search_interface/src/index.js
+++ lab/search_interface/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Demo from './Demo';
-import SearchButton from './Search/SearchButton';
+import SearchDialog from './Search/SearchDialog';
 import { libraries } from './libraries';
 
 const searchDemo = document.querySelector('#search-demo-react-root');
@@ -15,44 +15,17 @@ if (searchDemo) {
   );
 } else {
   try {
-    let { boostVersion, library } = parseURL();
+    const { boostVersion, library } = parseURL();
+    const searchButton = document.getElementById('gecko-search-button');
+    const currentBoostVersion = searchButton.getAttribute('data-current-boost-version');
+    const themeMode = searchButton.getAttribute('data-theme-mode');
 
-    const currentBoostVersion = document
-      .querySelector('script[data-current-version]')
-      .getAttribute('data-current-version')
-      .replaceAll('.', '_');
-
-    const div = Object.assign(document.createElement('div'), { id: 'search-button-react-root' });
-
-    // Workaround for gil and hana that have searchbox in their pages
-    if (library && (library.key === 'gil' || library.key === 'hana')) {
-      let searchBox = document.querySelector('#searchbox, #MSearchBox');
-      addCSS('#search-button-react-root {float: right; width: 120px; padding-right: 18px;}');
-      searchBox.replaceChildren(div);
-      // Workaround for spirit/classic and wave headers
-    } else if (library && (library.key === 'spirit/classic' || library.key === 'wave')) {
-      let td = document.querySelector('body > table:first-of-type td:nth-child(2)');
-      addCSS('#search-button-react-root {float: right; width: 120px;}');
-      td.append(div);
-    } else {
-      const heading = document.querySelector('#boost-common-heading-doc .heading-inner, #heading .heading-inner');
-      if (heading) {
-        addCSS('#search-button-react-root {float: right; width: 100px; padding-right: 18px;}');
-        addCSS('@media (max-device-width: 480px) {#search-button-react-root {padding-top: 18px;}}');
-        addCSS('#search-button-react-root * {color: #1976d2;}');
-        addCSS('#search-button-react-root button {background-color: #FFF;}');
-        heading.appendChild(div);
-      } else {
-        addCSS('#search-button-react-root button {background-color: #FFF;}');
-        addCSS('#search-button-react-root {width: 120px; top: 10px; right: 10px; position: absolute;}');
-        document.body.prepend(div);
-      }
-    }
-
+    const div = document.createElement('div');
     ReactDOM.createRoot(div).render(
       <React.StrictMode>
-        <SearchButton
-          versionWarning={boostVersion && boostVersion !== currentBoostVersion}
+        <SearchDialog
+          themeMode={themeMode}
+          versionWarning={!!boostVersion && boostVersion !== currentBoostVersion}
           library={library}
           urlPrefix={window.location.origin + `/doc/libs/${currentBoostVersion}`}
           algoliaIndex={currentBoostVersion}
@@ -62,10 +35,6 @@ if (searchDemo) {
       </React.StrictMode>,
     );
   } catch { }
-}
-
-function addCSS(css) {
-  document.head.appendChild(document.createElement('style')).innerHTML = css;
 }
 
 function parseURL() {
@@ -115,6 +84,8 @@ function parseURL() {
 
   if (!library && !specialPages.includes(window.location.pathname.replace(/\/+$/, '')))
     throw new Error(`Cannot extract the library_key from the URL`);
+
+  boostVersion = boostVersion.replace('boost_', '');
 
   return { boostVersion, library };
 }
